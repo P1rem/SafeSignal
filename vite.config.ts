237f300517f90e0063ee65240ed9904395e3218e
P1rem <premshelake77@gmail.com>
2026-09-22 22:53:46 +0530

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/SafeSignal/',
   plugins: [react()],
   resolve: {
     alias: {
